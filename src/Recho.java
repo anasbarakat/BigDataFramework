ca3cc3c2592c2e7c9c23adf0d33350f2000b57df
totocode <barakat9anas@gmail.com>
2017-09-29 23:12:32 +0200

    adding general comments to Recho function

--- src/Recho.java
+++ src/Recho.java
@@ -2,6 +2,8 @@ public class Recho {
 
     public static void main(String[] args) {
 
+        //principal case to treat: we begin from the end of the args tab
+        //and read teh elements in reverse
         if (args.length > 2) {
 
             for (int i = 1; i < args.length; i++) {
